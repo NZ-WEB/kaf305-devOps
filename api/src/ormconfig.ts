567db680fce43ff29e0abeee362ce1aefa468d92
NZ-WEB <stereo00@yandex.ru>
2022-02-25 12:21:20 +0300

    SET PROD docker-compose

--- api/src/ormconfig.ts
+++ api/src/ormconfig.ts
@@ -2,13 +2,9 @@ import { ConnectionOptions } from 'typeorm';
 
 const config: ConnectionOptions = {
   type: 'postgres',
-  host: 'localhost',
-  port: 5431,
-  username: 'kaf305_api',
-  password: '123',
-  database: 'kaf305_api',
+  url: process.env.DATABASE_URL,
   entities: [__dirname + '/**/*.entity{.ts,.js}'],
-  synchronize: false,
+  synchronize: true,
   migrations: [__dirname + '/migrations/**/*{.ts,.js}'],
   cli: {
     migrationsDir: 'src/migrations',
